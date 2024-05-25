517d53bf1cbd4aeadb75ddeb2cd4076815ae0c79
guzegui <guzmanegui@gmail.com>
2024-05-25 12:51:43 +0200

    Add dark mode context

---
+++ client/src/context/theme.context.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext();
+
+// CREATE A WRAPPER COMPONENT
+function ThemeProviderWrapper(props) {
+  return (
+    <ThemeContext.Provider value={"dark"}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+}
+
+export { ThemeContext, ThemeProviderWrapper }; // <== UPDATE
